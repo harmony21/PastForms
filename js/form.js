83afc30c25bf857a75571576532c2afdc2e4f251
Inna Botvinnik <21icelady97@gmail.com>
2018-02-04 13:45:35 +1000

    final version with adaptive

---
+++ js/form.js
@@ -0,0 +1,6 @@
+function ml_webform_success_7600178() {
+                var $ = ml_jQuery || jQuery;
+
+                $('.ml-subscribe-form-7600178 .ml-block-success').show();
+                $('.ml-subscribe-form-7600178 .ml-block-form').hide();
+            };
